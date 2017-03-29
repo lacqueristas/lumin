b107da2238d3578021787422fd1e2fa0040e09f7
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-03-28 20:43:42 -0700

    Okay so now we'll correctly set the Content-Type

--- source/worker/index.js
+++ source/worker/index.js
@@ -1,8 +1,10 @@
+import {PassThrough} from "stream"
 import requireEnvironmentVariables from "require-environment-variables"
 import {pipe} from "ramda"
 import {reduce} from "ramda"
 import {forEach} from "ramda"
-import gm from "gm"
+import magick from "gm"
+import fileTypeStream from "file-type-stream"
 
 import {zmqPullClient} from "../remote"
 import {logger} from "../remote"
@@ -20,29 +22,39 @@ const googleStorageBucket = googleCloudStorage.bucket(process.env.GOOGLE_CLOUD_S
 logger.info("Starting worker...")
 
 zmqPullClient()
-  .on("message", pipe(JSON.parse, ([id, names]) => {
-    const from = googleStorageBucket.file(`raw/${id}`).createReadStream()
-
+  .on("message", pipe(JSON.parse, ([id, names]: [string, string]) => {
+    const from = googleStorageBucket
+      .file(`raw/${id}`)
+      .createReadStream()
+    const graphicsMagick = magick(from)
     const applicableLenses = applicable(names)
 
     forEach(function processLenses ({name, pipeline}: LenseType) {
-      const magickClient = gm(from)
-      const chain = apply(pipeline)
-      const to = googleStorageBucket.file(`processed/${id}/${name}`).createWriteStream({
-        "gzip": true,
-        "cacheControl": "public, max-age=31556926",
-        "resumable": false,
-        "private": false,
-        "public": true,
-      })
+      const bridge = new PassThrough()
 
-      reduce((previous: any, current: Function): any => current(previous), magickClient, chain)
+      reduce((previous: any, current: Function): any => current(previous), graphicsMagick, apply(pipeline))
        .stream()
        .on("error", logger.error.bind(logger))
-       .on("end", () => {
-         logger.info(`Finished processing ${id}/${name}`)
-       })
-       .pipe(to)
+       .on("end", (): any => logger.info(`Finished processing ${id}/${name}`))
+       .pipe(
+         fileTypeStream(({mime}: {mime: string}) => {
+           bridge.pipe(
+             googleStorageBucket
+               .file(`processed/${id}/${name}`)
+               .createWriteStream({
+                 "gzip": true,
+                 "resumable": false,
+                 "private": false,
+                 "public": true,
+                 "metadata": {
+                   cacheControl: "public, max-age=31556926",
+                   contentType: mime,
+                 },
+               })
+           )
+         })
+       )
+       .pipe(bridge)
     }, applicableLenses)
   }))
   .on("error", logger.error)
